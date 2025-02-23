80a27281e63fbca7da1a70abc988242619d630e7
Moses Mugoya <mugoya.moses@students.mak.ac.ug>
2025-02-23 16:42:01 +0300

    Register models in the Django admin interface

--- server/app/admin.py
+++ server/app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User, Farm, Crop, Device, SensorData, CropRecommendation, FarmCrop
+
+admin.site.register(User)
+admin.site.register(Farm)
+admin.site.register(Crop)
+admin.site.register(Device)
+admin.site.register(SensorData)
+admin.site.register(CropRecommendation)
+admin.site.register(FarmCrop)
 
 # Register your models here.
