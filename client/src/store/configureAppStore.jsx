bb9427836685e7da3a5538617c52c210944ea822
heliprajapati <heli.prajapati02@gmail.com>
2025-04-11 15:36:15 +0300

    Added farm slice

--- client/src/store/configureAppStore.jsx
+++ client/src/store/configureAppStore.jsx
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import farmslice from './farm';
 
 export const store = configureStore({
-    reducer: {},    
+    reducer: {
+        farm: farmslice.reducer,
+    },    
 });
